2a40eb59a4e02381ce240bb994a3dbf46424c1ed
Seungyeon Han <145998828+mishapsnapsandtrickery@users.noreply.github.com>
2025-06-20 17:35:40 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,12 +67,3 @@ else:
         with st.chat_message("assistant"):
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
-
-
-st.markdown("""
-    <style>
-    html, body, .stApp {
-        background-color: #f7f5bc;
-    }
-    </style>
-""", unsafe_allow_html=True)
